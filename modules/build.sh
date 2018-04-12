08fcf2e46c2a9ed9f435c85bfc8c918f64eabb25
Michael Clausen <encodeering@gmail.com>
2018-04-12 20:49:29 +0200

    initializes project with alpine image
    * setup uses latest ci package

---
+++ modules/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+import com.encodeering.ci.config
+import com.encodeering.ci.docker
+
+docker-pull "$REPOSITORY/alpine-$ARCH:3.7" "alpine:3.7"
+
+docker-build alpine
+
+docker-verify
