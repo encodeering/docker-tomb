1555d2d725a62a6be584eb744265a6a51d0111da
Michael Clausen <encodeering@gmail.com>
2019-06-02 11:48:14 +0200

    updates alpine from 3.7 to 3.9
    * latest release

--- modules/build.sh
+++ modules/build.sh
@@ -5,7 +5,7 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/alpine-$ARCH:3.7" "alpine:3.7"
+docker-pull "$REPOSITORY/alpine-$ARCH:3.9" "alpine:3.9"
 
 docker-build --build-arg TOMB=$VERSION alpine
 
